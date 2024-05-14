ec94f27a31de36b7e79c902ed156cd8142f2f5f6
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2024-05-14 22:16:20 +0000

    Deploying to gh-pages from @ RedstoneWizard08/DocumentationWarning@5cbbaa7cd84304d26efcfc18a6603f281d270026 🚀

---
+++ docs/Subnautica/public/main.js
@@ -0,0 +1,29 @@
+export default {
+    defaultTheme: "dark",
+    iconLinks: [
+        
+        {
+            icon: "globe",
+            href: "https://unknownworlds.com/subnautica/",
+            title: "Website",
+        },
+        
+        {
+            icon: "steam",
+            href: "https://store.steampowered.com/app/264710/Subnautica/",
+            title: "Steam Page",
+        },
+        {
+            icon: "github",
+            href: "https://github.com/RedstoneWizard08/DocumentationWarning",
+            title: "GitHub",
+        },
+        
+        {
+            icon: "download",
+            href: "https://thunderstore.io/c/subnautica/",
+            title: "Thunderstore",
+        },
+        
+    ],
+};
