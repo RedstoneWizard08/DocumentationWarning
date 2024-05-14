c2bd8ac1260df2596e6eae40ddc4e0d37c5d078c
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2024-05-14 01:28:18 +0000

    Deploying to gh-pages from @ RedstoneWizard08/DocumentationWarning@16531af9e052d7e568da1dc5f48987192cc0cb04 🚀

---
+++ docs/LethalCompany/public/main.js
@@ -0,0 +1,23 @@
+export default {
+    defaultTheme: "dark",
+    iconLinks: [
+        
+        {
+            icon: "steam",
+            href: "https://store.steampowered.com/app/1966720/Lethal_Company/",
+            title: "Steam Page",
+        },
+        {
+            icon: "github",
+            href: "https://github.com/RedstoneWizard08/DocumentationWarning",
+            title: "GitHub",
+        },
+        
+        {
+            icon: "download",
+            href: "https://thunderstore.io/c/lethal-company/",
+            title: "Thunderstore",
+        },
+        
+    ],
+};
