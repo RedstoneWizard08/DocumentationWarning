0af2589076566ffbde2618c8bc7109f1b812c557
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2024-05-14 00:46:18 +0000

    Lower

--- Extractor/Source/DocItem.cs
+++ Extractor/Source/DocItem.cs
@@ -1,7 +1,12 @@
+using Newtonsoft.Json;
+
 namespace Extractor;
 
 public class DocItem
 {
+    [JsonProperty("id")]
     public required string Id;
+
+    [JsonProperty("name")]
     public required string Name;
 }
