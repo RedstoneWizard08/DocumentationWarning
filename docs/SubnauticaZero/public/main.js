1cbcbae15af5de2b29ed89123787b6bf63a72f5a
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2024-05-15 03:32:22 +0000

    Deploying to gh-pages from @ RedstoneWizard08/DocumentationWarning@bea80d29033344afc7b1dc3da3a1cc9c8d033f33 🚀

---
+++ docs/SubnauticaZero/public/main.js
@@ -0,0 +1,29 @@
+export default {
+    defaultTheme: "dark",
+    iconLinks: [
+        
+        {
+            icon: "globe",
+            href: "https://unknownworlds.com/subnautica/subnautica-below-zero/",
+            title: "Website",
+        },
+        
+        {
+            icon: "steam",
+            href: "https://store.steampowered.com/app/848450/Subnautica_Below_Zero/",
+            title: "Steam Page",
+        },
+        {
+            icon: "github",
+            href: "https://github.com/RedstoneWizard08/DocumentationWarning",
+            title: "GitHub",
+        },
+        
+        {
+            icon: "download",
+            href: "https://thunderstore.io/c/subnautica-below-zero/",
+            title: "Thunderstore",
+        },
+        
+    ],
+};
