9d10f56d8cda69eeb991dc7ca4c20caf3b6fa739
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2025-05-23 03:19:11 +0000

    Deploying to gh-pages from @ RedstoneWizard08/DocumentationWarning@01644583a850fbec2fa9d567d8b6b3c20514d7da 🚀

---
+++ docs/Stationeers/public/main.js
@@ -0,0 +1,23 @@
+export default {
+    defaultTheme: "dark",
+    iconLinks: [
+        
+        {
+            icon: "globe",
+            href: "https://www.stationeers.com/",
+            title: "Website",
+        },
+        
+        {
+            icon: "steam",
+            href: "https://store.steampowered.com/app/544550/",
+            title: "Steam Page",
+        },
+        {
+            icon: "github",
+            href: "https://github.com/RedstoneWizard08/DocumentationWarning",
+            title: "GitHub",
+        },
+        
+    ],
+};
