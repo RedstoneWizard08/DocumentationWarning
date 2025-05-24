8a019d88cdb4c7ce0532538b47d0bc95fe266eab
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2025-05-24 05:40:29 +0000

    Deploying to gh-pages from @ RedstoneWizard08/DocumentationWarning@2a3b847239017f05469f161a81491abbb6ec6bf9 🚀

---
+++ docs/SpaceEngineers/public/main.js
@@ -0,0 +1,23 @@
+export default {
+    defaultTheme: "dark",
+    iconLinks: [
+        
+        {
+            icon: "globe",
+            href: "https://www.spaceengineersgame.com/",
+            title: "Website",
+        },
+        
+        {
+            icon: "steam",
+            href: "https://store.steampowered.com/app/244850/",
+            title: "Steam Page",
+        },
+        {
+            icon: "github",
+            href: "https://github.com/RedstoneWizard08/DocumentationWarning",
+            title: "GitHub",
+        },
+        
+    ],
+};
