d76a0acdbb1c18779cc2348ca9affe881a9b00ec
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2025-12-01 23:46:59 +0000

    Deploying to gh-pages from @ RedstoneWizard08/DocumentationWarning@482057b56137897e34425e333f65f72d2afffe62 🚀

---
+++ docs/KSP2/public/main.js
@@ -0,0 +1,23 @@
+export default {
+    defaultTheme: "dark",
+    iconLinks: [
+        
+        {
+            icon: "globe",
+            href: "https://www.kerbalspaceprogram.com/",
+            title: "Website",
+        },
+        
+        {
+            icon: "steam",
+            href: "https://store.steampowered.com/app/954850/Kerbal_Space_Program_2/",
+            title: "Steam Page",
+        },
+        {
+            icon: "github",
+            href: "https://github.com/RedstoneWizard08/DocumentationWarning",
+            title: "GitHub",
+        },
+        
+    ],
+};
